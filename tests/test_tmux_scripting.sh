d09629cae25b4f510b6c0905f93723f77ca4100c
Robert Estelle <robertestelle@gmail.com>
2023-09-08 12:15:24 -0700

    lint: separately declare and assign local

--- tests/test_tmux_scripting.sh
+++ tests/test_tmux_scripting.sh
@@ -15,7 +15,8 @@ main() {
 	tmux new -d
 	tmux new-window
 
-	local number_of_windows="$(number_of_windows)"
+	local number_of_windows
+	number_of_windows="$(number_of_windows)"
 	if [[ "$number_of_windows" -ne 2 ]]; then
 		fail_helper "Incorrect number of windows. Expected 2, got $number_of_windows"
 	fi
