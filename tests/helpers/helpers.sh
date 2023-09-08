a9e216feb1866e3062507bd06f497310e66b6a62
Robert Estelle <robertestelle@gmail.com>
2023-09-08 15:27:41 -0700

    helpers: use rev-parse --show-superproject-working-tree to get parent repo

--- tests/helpers/helpers.sh
+++ tests/helpers/helpers.sh
@@ -12,7 +12,15 @@ TEST_STATUS='success'
 _clone_the_plugin() {
 	local plugin_path="${HOME}/.tmux/plugins/tmux-plugin-under-test/"
 	rm -rf "$plugin_path"
-	git clone --recursive "${CURRENT_DIR}/../" "$plugin_path" >/dev/null 2>&1
+
+	# Resolve symlinks to this file in the `tmux-test` repo.
+	local tmux_test_helpers_script
+	tmux_test_helpers_script="$(realpath "${BASH_SOURCE[0]}")"
+
+	# Get the path of the parent repository containing `tmux-test`.
+	local plugin_src_repo
+	plugin_src_repo="$(git -C "$(dirname "${tmux_test_helpers_script}")" rev-parse --show-superproject-working-tree)"
+	git clone --recursive "$plugin_src_repo" "$plugin_path" >/dev/null 2>&1
 }
 
 _add_plugin_to_tmux_conf() {
