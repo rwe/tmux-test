cf7aba1e380db7f016441ebfe7b402c9e49b1303
Robert Estelle <robertestelle@gmail.com>
2023-09-09 13:33:24 -0700

    count windows with grep -c . and avoid masked error status

--- tests/test_tmux_scripting.sh
+++ tests/test_tmux_scripting.sh
@@ -5,9 +5,9 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers/helpers.sh"
 
 number_of_windows() {
-	tmux list-windows |
-		wc -l |
-		sed 's/ //g'
+	local listed_windows
+	listed_windows="$(tmux list-windows)"
+	\grep -c . <<< "$listed_windows"
 }
 
 main() {
